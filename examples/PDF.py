93456c63ef919b1f1b9ef7663da2a6ecf2410dfe
SeedrsWill <101575979+SeedrsWill@users.noreply.github.com>
2022-04-08 15:11:29 +0100

    Delete PDF.py

--- examples/PDF.py
+++
@@ -1,6 +0,0 @@
-import tabula.io
-import pandas as pd
-df = tabula.io.convert_into('XLDocs', 'XLDocs', output_format="csv", java_options=None)
-
-for i in range(len(df)):
- df[i].to_excel('CERT_'+str(i)+'.xlsx')
